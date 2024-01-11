74df56bde6c1707dcd27f835ccc50de46ae8e58a
GeonWoo <gdbsrjsdn@gmail.com>
2024-01-11 13:19:17 +0900

    Feat: item-service repo 생성 item service project build

---
+++ src/main/java/kea/dpang/item/ItemServerApplication.java
@@ -0,0 +1,13 @@
+package kea.dpang.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemServerApplication.class, args);
+    }
+
+}
