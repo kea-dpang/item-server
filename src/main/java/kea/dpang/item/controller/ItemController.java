6193663c271c216c89af4bcc28a9b83f2f7288ae
GeonWoo <gdbsrjsdn@gmail.com>
2024-01-22 14:40:34 +0900

    Feat: 상품 구현 완료 (필터 제외) Review 구현 예정

--- src/main/java/kea/dpang/item/controller/ItemController.java
+++ src/main/java/kea/dpang/item/controller/ItemController.java
@@ -1,11 +1,7 @@
 package kea.dpang.item.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import kea.dpang.item.dto.CreateItemDto;
-import kea.dpang.item.dto.ItemDetailDto;
-import kea.dpang.item.dto.PopularItemDto;
-import kea.dpang.item.dto.UpdateItemDto;
-import kea.dpang.item.entity.Item;
+import kea.dpang.item.dto.*;
 import kea.dpang.item.service.ItemServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -33,14 +28,23 @@ public class ItemController {
         return ResponseEntity.ok(item);
     }
 
+    @GetMapping("/{itemId}")
+    @Operation(summary = "상품 리스트 조회", description = "상품의 썸네일 정보를 조회합니다.")
+    public ResponseEntity<ItemThumbnailDto> getItemThumbnail(@PathVariable Long itemId) {
+        ItemThumbnailDto item = itemService.getItemThumbnail(itemId);
+        log.info("상품 리스트 조회 완료. 상품 ID: {}", item.getItemId());
+
+        return ResponseEntity.ok(item);
+    }
+
     @GetMapping("/{itemId}/popular")
     @Operation(summary = "인기 상품 조회", description = "인기 상품을 조회합니다.")
     public ResponseEntity<ItemDetailDto> getPopularItems(@PathVariable Long itemId, Double score) {
         ItemDetailDto item = itemService.getPopularItems(itemId, score);
         log.info("인기 상품 정보 조회 완료. 상 ID: {}", item.getItemId());
+
         return ResponseEntity.ok(item);
     }
-
 
     @PostMapping
     @Operation(summary = "상품 등록", description = "상품 정보를 시스템에 추가합니다.")
@@ -76,8 +80,9 @@ public class ItemController {
 
     @PostMapping("/{itemId}/view")
     @Operation(summary = "조회수 증가", description = "조회 시 조회수가 증가되게 합니다.")
-    public ResponseEntity<Void> incrementItemView(@PathVariable Long id) {
-        itemService.incrementItemViewCount(id);
+    public ResponseEntity<Void> incrementItemView(@PathVariable Long itemId) {
+        itemService.incrementItemViewCount(itemId);
+
         return ResponseEntity.ok().build();
     }
 }
