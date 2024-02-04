5cfd9b324bf3b53e09b6117ac6a0cbe6411d9136
SeungHyeon <namsh1125@naver.com>
2024-02-05 02:11:15 +0900

    Refactor: 불필요한 서비스단 제거

--- src/main/java/kea/dpang/item/service/ItemService.java
+++ src/main/java/kea/dpang/item/service/ItemService.java
@@ -1,13 +1,9 @@
 package kea.dpang.item.service;
 
-import kea.dpang.item.dto.Item.*;
-import kea.dpang.item.entity.Category;
-import kea.dpang.item.entity.Item;
-import kea.dpang.item.entity.SubCategory;
-import kea.dpang.item.feign.dto.ItemSimpleListDto;
+import kea.dpang.item.dto.Item.ItemCreateDto;
+import kea.dpang.item.dto.Item.ItemResponseDto;
+import kea.dpang.item.dto.Item.ItemUpdateDto;
 import kea.dpang.item.dto.Stock.StockManageDto;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -30,27 +26,9 @@ public interface ItemService {
     ItemResponseDto getItem(Long itemId);
 
     /**
-     * 상품 카드 목록을 페이지 정보에 따라 조회합니다.
-     *
-     * @param pageable 페이지 정보
-     * @return 조회된 상품 카드 목록이 담긴 DTO 리스트
-     */
-    List<ItemCardDto> getItemCard(Pageable pageable);
-
-    /**
-     * 관리자용 상품 목록을 페이지 정보에 따라 조회합니다.
-     *
-     * @param pageable 페이지 정보
-     * @return 조회된 상품 목록이 담긴 DTO 리스트
-     */
-   Page<ItemManageListDto> getItemManageList(Pageable pageable);
-
-    Page<ItemCardDto> filterItems(Category category, SubCategory subCategory, Double minPrice, Double maxPrice, String keyword, Pageable pageable);
-
-    /**
      * 상품의 정보를 업데이트합니다.
      *
-     * @param itemId 업데이트할 상품의 ID
+     * @param itemId        업데이트할 상품의 ID
      * @param itemUpdateDto 업데이트할 상품의 정보가 담긴 DTO
      * @return 업데이트된 상품의 정보가 담긴 Detail DTO
      */
@@ -64,40 +42,10 @@ public interface ItemService {
     void deleteItem(List<Long> itemIds);
 
     /**
-     * 인기 상품을 조회합니다.
-     *
-     * @return 조회된 인기 상품 목록이 담긴 DTO 리스트
-     */
-    List<PopularItemDto> getPopularItems();
-
-    /**
-     * 주어진 ID에 해당하는 상품의 재고 수량을 조회합니다.
-     *
-     * @param itemId 재고 수량을 조회할 상품의 ID
-     * @return 조회된 재고 수량
-     */
-    int getStockQuantity(Long itemId);
-
-    /**
      * 주어진 ID에 해당하는 상품의 재고 수량을 업데이트 합니다.
      *
-     * @param itemId 재고 수량을 수정시킬 상품의 ID
+     * @param itemId   재고 수량을 수정시킬 상품의 ID
      * @param quantity 수정시킬 재고 수량
      */
     StockManageDto changeStock(Long itemId, int quantity);
-
-    /* feign */
-    // 이벤트
-    String getItemName(Long ItemId);
-
-    // 주문
-    Item getItemInquiry(Long itemId);
-
-    // 사용자
-    /**
-     * 장바구니, 위시리스트 // 백엔드용 상품 리스트 조회
-     *
-     * @return 조회된 모든 상품 목록이 담긴 DTO 리스트
-     */
-    List<ItemSimpleListDto> getCartItemsInquiry(List<Long> itemIds);
 }
--- src/main/java/kea/dpang/item/service/ItemServiceImpl.java
+++ src/main/java/kea/dpang/item/service/ItemServiceImpl.java
@@ -1,28 +1,20 @@
 package kea.dpang.item.service;
 
-import kea.dpang.item.dto.Item.*;
-import kea.dpang.item.entity.Category;
+import kea.dpang.item.dto.Item.ItemCreateDto;
+import kea.dpang.item.dto.Item.ItemResponseDto;
+import kea.dpang.item.dto.Item.ItemUpdateDto;
+import kea.dpang.item.dto.Stock.StockManageDto;
 import kea.dpang.item.entity.Item;
-import kea.dpang.item.entity.SubCategory;
 import kea.dpang.item.exception.ItemNotFoundException;
 import kea.dpang.item.feign.SellerServiceFeignClient;
-import kea.dpang.item.feign.dto.ItemSimpleListDto;
-import kea.dpang.item.dto.Stock.StockManageDto;
 import kea.dpang.item.repository.ItemRepository;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -52,54 +44,6 @@ public class ItemServiceImpl implements ItemService {
         return item.toItemResponseDto(sellerName);
     }
 
-    // 상품 카드 리스트 조회
-    @Override
-    @Transactional
-    public List<ItemCardDto> getItemCard(Pageable pageable) {
-        Page<Item> items = itemRepository.findAll(pageable);
-        return items.stream()
-                .map(ItemCardDto::new)
-                .collect(Collectors.toList());
-    }
-
-    // 관리자용 상품 리스트 조회
-    @Override
-    @Transactional
-    public Page<ItemManageListDto> getItemManageList(Pageable pageable) {
-        Page<Item> items = itemRepository.findAll(pageable);
-//        return items.stream()
-//                .map(ItemManageListDto::new)
-//                .collect(Collectors.toList());
-        return items.map(ItemManageListDto::new);
-    }
-
-    // 인기 상품 조회
-    @Override
-    @Transactional(readOnly = true)
-    public List<PopularItemDto> getPopularItems() {
-        // Redis에서 조회수를 기준으로 인기 상품 ID와 점수를 가져옴.
-        Set<ZSetOperations.TypedTuple<String>> items = redisTemplate.opsForZSet()
-                .reverseRangeWithScores(ITEM_VIEW_COUNT_KEY, 0, -1);
-
-        // 가져온 데이터를 PopularItemDto 리스트로 변환.
-        return items.stream().map(item -> {
-            Long itemId = Long.valueOf(item.getValue());
-            Double score = item.getScore();
-            String itemName = "Item " + itemId;
-
-            return new PopularItemDto(itemId, itemName, score);
-        }).toList();
-    }
-
-    // 상품 검색
-    @Override
-    @Transactional
-    public Page<ItemCardDto> filterItems(Category category, SubCategory subCategory, Double minPrice, Double maxPrice, String keyword, Pageable pageable) {
-        Page<Item> items = itemRepository.filterItems(category, subCategory, minPrice, maxPrice, keyword, pageable);
-        return items.map(ItemCardDto::new);
-    }
-
-
     // 상품 수정
     @Override
     @Transactional
@@ -113,22 +57,13 @@ public class ItemServiceImpl implements ItemService {
     @Override
     @Transactional
     public void deleteItem(List<Long> itemIds) {
-        for (Long itemId:itemIds){
+        for (Long itemId : itemIds) {
 
             Item item = itemRepository.findById(itemId)
                     .orElseThrow(() -> new ItemNotFoundException(itemId));
             itemRepository.delete(item);
         }
 
-    }
-
-    // 재고 수량 조회
-    @Override
-    @Transactional
-    public int getStockQuantity(Long itemId) {
-        Item item = itemRepository.findById(itemId)
-                .orElseThrow(() -> new ItemNotFoundException(itemId));
-        return item.getStockQuantity();
     }
 
     // 재고 수량 증감
@@ -144,30 +79,4 @@ public class ItemServiceImpl implements ItemService {
                 .build();
     }
 
-    /* feign */
-    // 이벤트 - 상품명 조회
-    @Override
-    @Transactional
-    public String getItemName(Long itemId) {
-        return itemRepository.findById(itemId)
-                .orElseThrow(() -> new ItemNotFoundException(itemId)).getItemName();
-    }
-
-    // 주문 - 상품 정보 조회
-    @Override
-    @Transactional(readOnly = true)
-    public Item getItemInquiry(Long itemId) {
-        return itemRepository.findById(itemId)
-                .orElseThrow(() -> new ItemNotFoundException(itemId));
-    }
-
-    // 장바구니, 위시리스트 - 상품 리스트 조회
-    @Override
-    @Transactional
-    public List<ItemSimpleListDto> getCartItemsInquiry(List<Long> itemId) {
-        List<Item> items = itemRepository.findAllByItemIdIn(itemId);
-        return items.stream()
-                .map(ItemSimpleListDto::new)
-                .collect(Collectors.toList());
-    }
 }
