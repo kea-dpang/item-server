608ce733ab213cd9d6d9b62e6a8e8785e718e47e
SeungHyeon <namsh1125@naver.com>
2024-02-05 19:01:08 +0900

    Update: 판매처명 가져오는 로직 수정

--- src/main/java/kea/dpang/item/feign/SellerServiceFeignClient.java
+++ src/main/java/kea/dpang/item/feign/SellerServiceFeignClient.java
@@ -1,15 +1,16 @@
 package kea.dpang.item.feign;
 
 import kea.dpang.item.base.SuccessResponse;
+import kea.dpang.item.feign.dto.SellerDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "seller-server")
 public interface SellerServiceFeignClient {
 
     // 판매처 이름을 받아오기 위한 API
-    @GetMapping("/api/seller/findName")
-    ResponseEntity<SuccessResponse<String>> getSeller(@RequestParam Long id);
+    @GetMapping("/api/seller/{id}")
+    ResponseEntity<SuccessResponse<SellerDto>> getSeller(@PathVariable Long id);
 }
