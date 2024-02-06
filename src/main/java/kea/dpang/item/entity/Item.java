1a05edfd5b75227cf5ab1c25712ff64812fcd5fb
GeonWoo <gdbsrjsdn@gmail.com>
2024-02-06 11:54:31 +0900

    Feat: Add image Entity for @ElementCollection -> @OneToMany

--- src/main/java/kea/dpang/item/entity/Item.java
+++ src/main/java/kea/dpang/item/entity/Item.java
@@ -69,8 +69,8 @@ public class Item extends BaseEntity {
     private String thumbnailImage;
 
     // 상품 정보 이미지 리스트
-    @ElementCollection
-    private List<String> informationImages;
+    @OneToMany(mappedBy = "item", cascade = CascadeType.ALL)
+    private List<Image> informationImages;
 
     public void update(UpdateItemRequestDto dto) {
         this.name = dto.getItemName();
