146db0d2d75080b93041fae4bf5e58cebbfa08a6
GeonWoo <gdbsrjsdn@gmail.com>
2024-02-06 19:32:24 +0900

    Feat: Add ItemName in used by PersonalReviewDto

--- src/main/java/kea/dpang/item/dto/review/PersonalReviewDto.java
+++ src/main/java/kea/dpang/item/dto/review/PersonalReviewDto.java
@@ -13,22 +13,24 @@ public class PersonalReviewDto {
     private Long reviewId;
     private Long reviewerId;
     private String reviewerName;
-    private LocalDateTime createdTime;
     private Long itemId;
-    private String itemImage;
+    private String itemName;
+    private String itemThumbnailImage;
     private String content;
     private Double rating;
+    private LocalDateTime createdTime;
 
     public static PersonalReviewDto of(Review review, String reviewerName) {
         return new PersonalReviewDto(
                 review.getReviewId(),
                 review.getReviewerId(),
                 reviewerName,
-                review.getCreatedTime(),
                 review.getItem().getId(),
+                review.getItem().getName(),
                 review.getItem().getThumbnailImage(),
                 review.getContent(),
-                review.getRating()
+                review.getRating(),
+                review.getCreatedTime()
         );
     }
 }
